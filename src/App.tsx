232abae278374f1b8fc3bb432b5d61bccaeb9940
xiaoguaishou1 <760695955@qq.com>
2024-09-27 11:59:50 +0800

    dev2

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>dev1</h1>
+      <h1>dev2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
