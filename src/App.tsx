d63ab9016ce5f266285f4648718e7667330b065f
xiaoguaishou1 <760695955@qq.com>
2024-09-27 12:11:58 +0800

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: xiaoguaishou1 760695955@qq.com
  * @Date: 2024-09-27 11:57:01
  * @LastEditors: xiaoguaishou1 760695955@qq.com
- * @LastEditTime: 2024-09-27 11:59:24
+ * @LastEditTime: 2024-09-27 12:11:52
  * @FilePath: /gitTest/src/App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -24,7 +24,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>dev3</h1>
+      <h1>dev</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
